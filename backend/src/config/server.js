865bf8d0060dd373b3b50031a20cfbcb53a419f9
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-07 19:38:10 -0300

    query parser int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('../config/cors')
+const queryPaser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryPaser())
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`)
