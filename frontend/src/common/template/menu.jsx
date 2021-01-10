0077432ac44da019a9f94de03107ed0e418a06da
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-10 00:30:48 -0300

    Side bar menu

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import MenuItem from '../template/menuItem.jsx'
+import MenuTree from '../template/menuTree.jsx'
 
 
 export default props => {
     return (
         <ul className="sidebar-menu">
+            <MenuItem path="#" label="Dashboard" icon='dashboard'/>
+            <MenuTree label="Cadastro" icon="edit">
+                <MenuItem path="#billingCycles"
+                label="Ciclos de pagamentos" icon="usd"
+                />
+                
+            </MenuTree>
 
         </ul>
 
