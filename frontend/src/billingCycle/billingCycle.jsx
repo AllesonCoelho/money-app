ee0609fee35b6cf33ed8f75e058ed18981309e66
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-13 01:14:28 -0300

    Tab Component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/tab/tabs.jsx'
 import TabsHeader from '../common/tab/tabsHeader.jsx'
 import TabsContent from '../common/tab/tabsContent.jsx'
 import TabHeader from '../common/tab/tabHeader.jsx'
+import TabContent from '../common/tab/tabContent.jsx'
 
 
 class BillingCycle extends Component{
@@ -24,6 +25,10 @@ class BillingCycle extends Component{
 
                         </TabsHeader>
                         <TabsContent>
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
 
                         </TabsContent>
                     </Tabs>
