f4ad520d431817b530e06bd1325781a51cd04b18
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-13 19:13:53 -0300

    Action getList

--- frontend/src/main/reducer.js
+++ frontend/src/main/reducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer.js'
 import TabReducer from '../common/tab/tabReducer.js'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer.js'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 
