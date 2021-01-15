744088aca2684d88a8d18d80a15b6342ef89161d
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-15 19:51:44 -0300

    Melhorias no submit do ciclo de pagamento

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,28 +1,33 @@
 import axios from 'axios'
 import { template } from 'lodash'
-import {toastr} from 'react-redux-toastr'
+import { toastr } from 'react-redux-toastr'
+import {reset as resetForm} from 'redux-form'
+import {showTabs, selectTab} from '../common/tab/tabActions.js'
 const BASE_URL = 'http://localhost:3003/api'
 
 
-export function getList(){
+export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
-    return{
+    return {
         type: 'BILLING_CICLES_FETCHED',
         payload: request
     }
 }
 
 
-export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    .then(resp => {
-        toastr.success('Sucesso', 'Operacao Realizada com Sucesso')
-    }).catch(e => {
-        e.response.data.errors.forEach(error => toastr.error('Erro', error))
-    })
-    return{
-        type: 'TEMP',
-
+export function create(values) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operacao Realizada com Sucesso')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            }).catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
-
 }
