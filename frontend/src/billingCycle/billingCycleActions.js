7b0e4bc6ce72effd14850b2aa85f7e22a445a2f2
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-14 22:45:24 -0300

    Mensagens de sucesso e erro

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { template } from 'lodash'
+import {toastr} from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api'
 
 
@@ -14,6 +15,11 @@ export function getList(){
 
 export function create(values){
     axios.post(`${BASE_URL}/billingCycles`, values)
+    .then(resp => {
+        toastr.success('Sucesso', 'Operacao Realizada com Sucesso')
+    }).catch(e => {
+        e.response.data.errors.forEach(error => toastr.error('Erro', error))
+    })
     return{
         type: 'TEMP',
 
