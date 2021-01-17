435bedae300cc44cb7567d53a2ad13eefc6cd2f2
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-16 21:18:21 -0300

    Componente Field Atualizado

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {reduxForm, Field} from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput.jsx'
 
 class BillingCycleForm extends Component{
     render(){
@@ -8,9 +9,16 @@ class BillingCycleForm extends Component{
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name='name' component='input'/>
-                    <Field name='month' component='input'/>
-                    <Field name='year' component='input'/>
+                    <Field name='name' component={labelAndInput}
+                    label='Nome' cols='12 4' placeholder='Informe o nome'
+                    />
+                    <Field name='month' component={labelAndInput} type='number'
+                    label='Mes' cols='12 4' placeholder='Informe o mes'
+                    />
+                    <Field name='year' component={labelAndInput} type='number'
+                    label='Ano' cols='12 4' placeholder='Informe o ano'
+                    
+                    />
 
                 </div>
                 <div className="box-footer">
