95c5f2a65d0223824e10f513fc35fd505e83062b
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-19 03:04:10 -0300

    Componente Credit LIst

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import Grid from '../common/layout/grid'
 import { Field } from 'redux-form'
+import Input from '../common/form/input.jsx'
 
 class CreditList extends Component {
 
     renderRows() {
         return (
             <tr>
-                <td><Field name='debts[0].name' component='input' /></td>
-                <td><Field name='debts[0].value' component='input' /></td>
+                <td><Field name='debts[0].name' component={Input}
+                placeholder='Informe o nome' readOnly={this.props.readOnly}
+                /></td>
+                <td><Field name='debts[0].value' component={Input}
+                placeholder='Informe o valor' readOnly={this.props.readOnly}
+                /></td>
                 <td></td>
             </tr>
         )
